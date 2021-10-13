34a5dc4203ebbb6820ec94fb7a95693878ed9f1e
Dominik Kröll <Dominik.Kroell@mni.thm.de>
2021-10-14 01:28:24 +0200

    database changes.

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,19 +1,21 @@
-create table valid_tokens (
+create table access_token (
     token varchar(255) primary key,
     group_name varchar(255),
     count_case_studies int,
-    until timestamp
+    valid_until timestamp
 );
 
-create table case_studies (
+create table case_study (
     number int primary key,
     title varchar(255)
 );
 
-insert into valid_tokens values ('0000', 'Test Gruppe', 3, NOW() + '30 days'::interval);
-insert into valid_tokens values ('1111', 'Test Gruppe 2', 2, NOW() + '30 days'::interval);
+insert into access_token values ('0000', 'Test Gruppe', 3, NOW() + '30 days'::interval);
+insert into access_token values ('1111', 'Test Gruppe 2', 2, NOW() + '30 days'::interval);
 
 
-insert into case_studies values (1, 'Einzelhandel'),
-                                (2, 'Paketversand'),
-                                (3, 'Hotelbuchung');
+insert into case_study values (1, 'Einzelhandel'),
+                              (2, 'Paketversand'),
+                              (3, 'Hotelreservierung'),
+                              (4, 'Ticketverkauf'),
+                              (5, 'Pflegedienst');
