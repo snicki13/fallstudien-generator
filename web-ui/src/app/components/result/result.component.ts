1c377e7ce7fd562fbb1472e70aa538ca680062dc
Dominik Kröll <Dominik.Kroell@mni.thm.de>
2021-10-22 17:45:34 +0200

    Result

---
+++ web-ui/src/app/components/result/result.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core'
+import { CaseStudy } from '../../model/CaseStudy'
+import { StudentGroup } from '../../model/StudentGroup'
+
+@Component({
+  selector: 'app-result',
+  templateUrl: './result.component.html',
+  styleUrls: ['./result.component.css']
+})
+export class ResultComponent implements OnInit {
+  @Input() caseStudies: CaseStudy[] = []
+  @Input() groupInfo: StudentGroup
+
+  ngOnInit (): void {
+  }
+}
